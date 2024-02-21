aff13aed1878f464d59c62d6d70ffb48b5213a6d
ymkim <ji9@me.com>
2024-02-21 10:47:54 +0900

    chore: initialize

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ['lh3.googleusercontent.com']
-  }
-}
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
